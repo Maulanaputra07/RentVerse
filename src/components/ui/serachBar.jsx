b8cac1d6a98c3c7e802bdb120761fa3524c7c6d1
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2025-12-12 01:52:52 +0700

    hotfix: search

--- src/components/ui/serachBar.jsx
+++ src/components/ui/serachBar.jsx
@@ -2,7 +2,7 @@ import searchIcon from '/icons/search-normal.png'
 
 export default function SearchBar() {
     return (
-        <div className="px-2 py-3 flex items-center bg-white min-w-1/2 justify-center gap-5 rounded-xl shadow-xl">
+        <div className="md:px-2 md:py-3 py-2 flex items-center bg-white min-w-1/2 justify-center gap-5 rounded-xl shadow-xl">
             <div className="relative w-full">
                 <img 
                     src={searchIcon} 
