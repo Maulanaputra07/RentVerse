011bcf9b66c645c5a98378c971b3c54252797827
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2025-12-12 00:37:50 +0700

    config client api

--- src/api/client.js
+++ src/api/client.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const client = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    timeout: 40000,
-    withCredentials: true,
+    timeout: 10000,
 })
 
 export default client;
